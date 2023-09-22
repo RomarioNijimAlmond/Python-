74849160abf18e4ed4d1d01ba34355e1542c7556
RomarioNijim <romario@almond-qms.com>
2023-09-22 11:03:05 +0300

    RPS

--- part1/operators.py
+++ part1/operators.py
@@ -7,3 +7,16 @@ print(round_nums2)
 first_name = "Rom"
 last_name = "Nij"
 print(f"{first_name} {last_name}")
+
+# ternary operator
+
+number = 11
+print('true') if number > 10 else print('false')
+
+
+email = "example@gmail.com"
+print(email.split("@").pop())
+
+gpa = -12
+abso = abs(gpa)
+print(abso)
