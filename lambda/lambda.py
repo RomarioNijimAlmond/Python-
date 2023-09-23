5d8ec68385cbe3166efcbe837b1d010ddfdc5018
RomarioNijim <romario@almond-qms.com>
2023-09-23 16:21:33 +0300

    lambda

---
+++ lambda/lambda.py
@@ -0,0 +1,15 @@
+
+multiple = lambda num : num * num
+print(multiple(2))
+
+add = lambda num : num + num
+
+def addTwo(num): 
+    return num + 2
+
+print(addTwo(5))
+
+print(add(2))
+
+add_two_nums = lambda a,b : a + b
+print(add_two_nums(5,6))
