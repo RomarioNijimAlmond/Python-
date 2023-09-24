2b12cfd181580fa16117cb784098d8a8d18e7b98
RomarioNijim <romario@almond-qms.com>
2023-09-24 14:36:31 +0300

    pip and venv

---
+++ myvirtualenv/weather.py
@@ -0,0 +1,20 @@
+import requests
+from dotenv import load_dotenv
+import os 
+
+
+load_dotenv()
+
+def get_current_weather():
+    print("\n*** Get current wather conditions***\n")
+
+    city = input("\nPlease enter a city name:\n")
+    request_url = f"https://api.openweathermap.org/data/2.5/weather?&appid={os.getenv('API_KEY')}&q={city}&units=imperial"
+    # print(request_url)
+
+    weather_data = requests.get(request_url).json()
+    print(weather_data)
+
+
+
+get_current_weather()
