97c8fec0c3769eaac1320864535dd0bced13e7f6
RomarioNijim <romario@almond-qms.com>
2023-09-24 20:25:01 +0300

    read json file

--- files/file.py
+++ files/file.py
@@ -4,3 +4,12 @@ json_file_name = "random.json"
 with open(json_file_name) as json_file:
     data = json.load(json_file)
     print(data)
+
+    first_name = data["firstname"]
+    last_name = data["lastname"]
+    print(f"first name is: {first_name}")
+    print(f"last name is: {last_name}")
+
+
+
+
